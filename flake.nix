3c53fb7506a09b2b4d1716e93d2688fc243125e5
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-05-05 22:14:44 +0700

    feature - add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My dotfiles + Home Manager config";
+
+  outputs = { self, ... }: {
+    homeConfigurations.king = { pkgs, ... }: {
+      home.username = "vi-tran";
+      home.homeDirectory = "/home/vi-tran";
+
+      home.packages = with pkgs; [ 
+        godot 
+	gh 
+      ];
+
+      programs.git = {
+        enable = true;
+        userName = "vitran96";
+        userEmail = "38664902+vitran96@users.noreply.github.com";
+      };
+
+      # Symlink dotfiles (assuming they're in this repo)
+      home.file.".config/nvim".source = ./nvim;
+      #home.file.".bashrc".source = ./bashrc;
+
+      home.stateVersion = "24.11";
+    };
+  };
+}
+
